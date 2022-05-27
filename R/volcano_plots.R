11f674f62601d1c56570c67f472fd2bb29446520
Peter Carbonetto <peter.carbonetto@gmail.com>
2022-05-27 06:30:02 +0000

    version 0.6-135

---
+++ R/volcano_plots.R
@@ -0,0 +1,256 @@
+#' @rdname volcano_plot
+#'
+#' @title Volcano Plots for Visualizing Results of Differential Expression Analysis
+#'
+#' @description Create a \dQuote{volcano} plot to visualize the
+#'   results of a differential count analysis using a topic model. Here,
+#'   the volcano plot is a scatterplot in which the posterior mean
+#'   log-fold change (LFC), estimated by running the methods implemented
+#'   in \code{\link{de_analysis}}, is plotted against the estimated
+#'   z-score. Use \code{volcano_plotly} to create an interactive volcano
+#'   plot.
+#'
+#' @details Interactive volcano plots can be created using the
+#'   \sQuote{plotly} package. The \dQuote{hover text} shows the label
+#'   and detailed LFC statistics.
+#' 
+#' @param de An object of class \dQuote{topic_model_de_analysis},
+#'   usually an output from \code{\link{de_analysis}}. It is better to
+#'   run \code{de_analysis} with \code{shrink.method = "ash"} so that
+#'   the points in the volcano plot can be coloured by their local false
+#'   sign rate (lfsr).
+#'
+#' @param k The topic, selected by number or name.
+#'
+#' @param labels Character vector specifying how the points in the
+#'   volcano plot are labeled. This should be a character vector with
+#'   one entry per LFC estimate (row of \code{de$postmean}). When not
+#'   specified, the row names of \code{de$postmean} are used. When
+#'   available. labels are added to the plot using
+#'   \code{\link[ggrepel]{geom_text_repel}}.
+#'
+#' @param do.label The function used to deetermine which LFC estimates
+#'   to label. Replace \code{volcano_plot_do_label_default} with your
+#'   own function to customize the labeling of points in the volcano
+#'   plot.
+#' 
+#' @param ymax z-scores greater than \code{ymax} (in magnitude) are
+#'   shown as \code{max}. Setting \code{ymax} to a finite value can
+#'   improve the volcano plot when some z-scores are much larger (in
+#'   magnitude) than others.
+#' 
+#' @param max.overlaps Argument passed to
+#'   \code{\link[ggrepel]{geom_text_repel}}.
+#'
+#' @param plot.title The title of the plot.
+#' 
+#' @param ggplot_call The function used to create the plot. Replace
+#'   \code{volcano_plot_ggplot_call} with your own function to customize
+#'   the appearance of the plot.
+#'
+#' @return A \code{ggplot} object or a \code{plotly} object.
+#'
+#' @seealso \code{\link{de_analysis}}
+#' 
+#' @examples
+#' # See help(de_analysis) for examples.
+#' 
+#' @export
+#' 
+volcano_plot <-
+  function (de, k, labels, do.label = volcano_plot_do_label_default,
+            ymax = Inf, max.overlaps = Inf, plot.title = paste("topic",k),
+            ggplot_call = volcano_plot_ggplot_call) {
+  if (!inherits(de,"topic_model_de_analysis"))
+    stop("Input \"de\" should be an object of class ",
+         "\"topic_model_de_analysis\"")
+  n <- nrow(de$postmean)
+  if (missing(labels)) {
+    if (!is.null(rownames(de$postmean)))
+      labels <- rownames(de$postmean)
+    else
+      labels <- as.character(seq(1,n))
+  }
+  if (!(is.character(labels) & length(labels) == n))
+    stop("Input argument \"labels\", when specified, should be a character ",
+         "vector with one entry per log-fold change estimate (column of ",
+         "the counts matrix)")
+  dat <- compile_volcano_plot_data(de,k,ymax,labels,do.label)
+  return(ggplot_call(dat,plot.title,max.overlaps))
+}
+
+#' @rdname volcano_plot
+#'
+#' @param x An object of class \dQuote{topic_model_de_analysis},
+#'   usually an output from \code{\link{de_analysis}}.
+#'
+#' @param \dots Additional arguments passed to \code{volcano_plot}.
+#' 
+#' @importFrom graphics plot
+#' 
+#' @method plot topic_model_de_analysis
+#'
+#' @export
+#'
+plot.topic_model_de_analysis <- function (x, ...)
+  volcano_plot(x,...)
+
+#' @rdname volcano_plot
+#'
+#' @param file Save the interactive volcano plot to this HTML
+#'   file using \code{\link[htmlwidgets]{saveWidget}}.
+#' 
+#' @param width Width of the plot in pixels. Passed as argument
+#'   \dQuote{width} to \code{\link[plotly]{plot_ly}}.
+#'
+#' @param height Height of the plot in pixels. Passed as argument
+#'   \dQuote{height} to \code{\link[plotly]{plot_ly}}.
+#'
+#' @param plot_ly_call The function used to create the plot. Replace
+#'   \code{volcano_plot_ly_call} with your own function to customize
+#'   the appearance of the interactive plot.
+#'
+#' @importFrom htmlwidgets saveWidget
+#' 
+#' @export
+#' 
+volcano_plotly <- function (de, k, file, labels,
+                            ymax = Inf, width = 500, height = 500,
+                            plot.title = paste("topic",k),
+                            plot_ly_call = volcano_plot_ly_call) {
+
+  # Check and process input arguments.
+  if (!inherits(de,"topic_model_de_analysis"))
+    stop("Input \"de\" should be an object of class ",
+         "\"topic_model_de_analysis\"")
+  n <- nrow(de$postmean)
+  if (missing(labels)) {
+    if (!is.null(rownames(de$postmean)))
+      labels <- rownames(de$postmean)
+    else
+      labels <- as.character(seq(1,n))
+  }
+  if (!(is.character(labels) & length(labels) == n))
+    stop("Input argument \"labels\", when specified, should be a character ",
+         "vector with one entry per log-fold change estimate (column of ",
+         "the counts matrix)")
+  dat <- compile_volcano_plot_data(de,k,ymax,labels)
+  p <- volcano_plot_ly_call(dat,plot.title,width,height)
+  if (!missing(file))
+    saveWidget(p,file,selfcontained = TRUE,title = plot.title)
+  return(p)
+}
+
+#' @rdname volcano_plot
+#'
+#' @param lfc A vector of log-fold change estimates.
+#'
+#' @param z A vector of z-scores of the same length as \code{lfc}.
+#'
+#' @importFrom stats quantile
+#' 
+#' @export
+#' 
+volcano_plot_do_label_default <- function (lfc, z)
+  abs(z) >= quantile(abs(z),0.999,na.rm = TRUE) |
+    lfc <= quantile(lfc,0.001,na.rm = TRUE) |
+    lfc >= quantile(lfc,0.999,na.rm = TRUE)
+
+#' @rdname volcano_plot
+#'
+#' @param dat A data frame passed as input to
+#'   \code{\link[ggplot2]{ggplot}}, containing, at a minimum, columns
+#'   \dQuote{f0}, \dQuote{postmean}, \dQuote{z}, \dQuote{zabs},
+#'   \dQuote{lfsr} and \dQuote{label}.
+#' 
+#' @param font.size Font size used in plot.
+#'
+#' @importFrom ggplot2 ggplot
+#' @importFrom ggplot2 aes_string
+#' @importFrom ggplot2 geom_point
+#' @importFrom ggplot2 scale_y_continuous
+#' @importFrom ggplot2 labs
+#' @importFrom ggplot2 theme
+#' @importFrom ggplot2 element_text
+#' @importFrom ggrepel geom_text_repel
+#' @importFrom cowplot theme_cowplot
+#' 
+#' @export
+#' 
+volcano_plot_ggplot_call <- function (dat, plot.title, max.overlaps = Inf,
+                                      font.size = 9) {
+  dat$lfsr <- cut(dat$lfsr,c(-1,0.001,0.01,0.05,Inf))
+  return(ggplot(dat,aes_string(x = "postmean",y = "zabs",fill = "lfsr",
+                               label = "label")) +
+         geom_point(color = "white",stroke = 0.3,shape = 21,na.rm = TRUE) +
+         geom_text_repel(color = "darkgray",size = 2.25,fontface = "italic",
+                         segment.color = "darkgray",segment.size = 0.25,
+                         min.segment.length = 0,max.overlaps = max.overlaps,
+                         na.rm = TRUE) +
+         scale_y_continuous(trans = "sqrt",
+                            breaks = c(0,1,2,5,10,20,50,100,200,500,
+                                       1000,2000,5000,1e4,2e4,5e4)) +
+         scale_fill_manual(values = c("deepskyblue","gold","orange","coral")) +
+         labs(x = "log-fold change",y = "|z-score|",title = plot.title) +
+         theme_cowplot(font.size) +
+         theme(plot.title = element_text(size = font.size,face = "plain")))
+}
+
+#' @rdname volcano_plot
+#'
+#' @importFrom plotly plot_ly
+#' @importFrom plotly hide_colorbar
+#' @importFrom plotly layout
+#' 
+#' @export
+#' 
+volcano_plot_ly_call <- function (dat, plot.title, width, height) {
+  dat$fill <- cut(dat$lfsr,c(-1,0.001,0.01,0.05,Inf))
+  p <- plot_ly(data = dat,x = ~postmean,y = ~sqrt(zabs),color = ~fill,
+               colors = c("deepskyblue","gold","orange","coral"),
+               text = ~sprintf(paste0("%s\nmean(null): %0.2e\n",
+                                      "logFC(lower): %+0.3f\n",
+                                      "logFC(mean): %+0.3f\n",
+                                      "logFC(upper): %0.3f\n",
+                                      "z-score: %+0.3f\nlfsr: %0.2e"),
+                               label,f0,lower,postmean,upper,z,lfsr),
+               type = "scatter",mode = "markers",hoverinfo = "text",
+               width = width,height = height,
+               marker = list(line = list(color = "white",width = 1),
+                             size = 7.5))
+  p <- hide_colorbar(p)
+  p <- layout(p,
+              xaxis = list(title = "log-fold change",zeroline = FALSE,
+                           showgrid = FALSE),
+              yaxis = list(title = "sqrt|z-score|",zeroline = FALSE,
+                           showgrid = FALSE),
+              hoverlabel = list(bgcolor = "white",bordercolor = "black",
+                                font = list(color = "black",family = "arial",
+                                            size = 12)),
+              font = list(family = "arial",size = 12),
+              showlegend = FALSE,title = plot.title)
+  return(p)
+}
+
+# This is used by volcano_plot and volcano_plotly to compile the data
+# frame passed to 'ggplot'.
+compile_volcano_plot_data <- function (de, k, ymax, labels, do.label = NULL) {
+  if (all(is.na(de$lfsr))) {
+    message("lfsr is not available, probably because \"shrink.method\" was ",
+            "not set to \"ash\"; lfsr in plot should be ignored")
+    lfsr <- 0
+  } else
+    lfsr <- de$lfsr[,k]
+  dat <- data.frame(label    = labels,
+                    f0       = de$f0,
+                    lower    = de$lower[,k],
+                    postmean = de$postmean[,k],
+                    upper    = de$upper[,k],
+                    z        = de$z[,k],
+                    lfsr     = lfsr,
+                    stringsAsFactors = FALSE)
+  if (!is.null(do.label))
+    dat$label[which(!do.label(dat$postmean,dat$z))] <- ""
+  dat$zabs <- pmin(ymax,abs(dat$z))
+  return(dat)
+}
---
+++ README.md
@@ -0,0 +1,104 @@
+# fastTopics
+
+ [![R-CMD-check](https://github.com/stephenslab/fastTopics/workflows/R-CMD-check/badge.svg)](https://github.com/stephenslab/fastTopics/actions)
+[![CircleCI build status](https://circleci.com/gh/stephenslab/fastTopics.svg?style=svg)](https://app.circleci.com/pipelines/github/stephenslab/fastTopics)
+[![codecov](https://codecov.io/gh/stephenslab/fastTopics/branch/master/graph/badge.svg)](https://app.codecov.io/gh/stephenslab/fastTopics)
+
+fastTopics is an R package implementing fast, scalable optimization
+algorithms for fitting topic models ("grade of membership" models) and
+non-negative matrix factorizations to count data. The methods exploit
+the [special relationship][vignette-close-relationship] between the
+multinomial topic model (also "probabilistic latent semantic
+indexing") and Poisson non-negative matrix factorization. The package
+provides tools to compare, annotate and visualize model fits,
+including functions to efficiently create "structure plots" and
+identify key features in topics. The fastTopics package is a successor
+to the [CountClust package][countclust].
+
+If you find a bug, or you have a question or feedback on this software,
+please post an [issue][issues].
+
+## Citing this work
+
+If you find the fastTopics package or any of the source code in this
+repository useful for your work, please cite:
+
+> Kushal K. Dey, Chiaowen Joyce Hsiao and Matthew Stephens (2017).
+> [Visualizing the structure of RNA-seq expression data using grade of membership models.][countclust-paper]
+> *PLoS Genetics* **13**, e1006599.
+>
+> Peter Carbonetto, Kevin Luo, Kushal Dey, Joyce Hsiao and Matthew
+> Stephens (2021). fastTopics: fast algorithms for fitting topic models
+> and non-negative matrix factorizations to count data. R package
+> version 0.4-11. [https://github.com/stephenslab/fastTopics][fasttopics]
+
+## License
+
+Copyright (c) 2019-2021, Peter Carbonetto and Matthew Stephens.
+
+All source code and software in this repository are made available
+under the terms of the [MIT license][mit-license].
+
+## Quick Start
+
+Install and load the package:
+
+```R
+remotes::install_github("stephenslab/fastTopics")
+library(fastTopics)
+```
+
+Note that installing the package will require a C++ compiler setup
+that is appropriate for the version of R installed on your
+computer. For details, refer to the documentation on the
+[CRAN website][cran].
+
+For guidance on using fastTopics to analyze gene expression data, see
+the [single-cell RNA-seq vignette, part 1][vignette-scrnaseq-1] and
+[part 2][vignette-scrnaseq-2].
+
+Also, try running the small example that illustrates the fast model
+fitting algorithms:
+
+```R
+example("fit_poisson_nmf")
+```
+
+See the [package documentation][pkgdown] for more information.
+
+## Developer notes
+
+To prepare the package for CRAN, remove the "Remotes" field in the
+DESCRIPTION file, and set `on_cran <- TRUE` in `helper_functions.R`,
+then run `R CMD build fastTopics` to build the source package.
+
+This is the command used to check the package before submitting to
+CRAN:
+
+```r
+library(rhub)
+check_for_cran(".",show_status = TRUE,
+  env_vars = c(`_R_CHECK_FORCE_SUGGESTS_` = "false",
+               `_R_CHECK_CRAN_INCOMING_USE_ASPELL_` = "true"))
+```
+
+## Credits
+
+The fastTopics R package was developed by [Peter Carbonetto][peter],
+Kevin Luo, Kushal Dey, Joyce Hsiao and
+[Matthew Stephens][matthew] at the [University of Chicago][uchicago].
+
+[fasttopics]:  https://github.com/stephenslab/fastTopics
+[mit-license]: https://opensource.org/licenses/mit-license.html
+[issues]: https://github.com/stephenslab/fastTopics/issues
+[peter]: https://pcarbo.github.io
+[kevin]: https://github.com/kevinlkx
+[matthew]: http://stephenslab.uchicago.edu
+[uchicago]: https://www.uchicago.edu
+[cran]: https://cran.r-project.org
+[countclust]: https://github.com/kkdey/CountClust
+[countclust-paper]: https://doi.org/10.1371/journal.pgen.1006599
+[pkgdown]: https://stephenslab.github.io/fastTopics/
+[vignette-close-relationship]: https://stephenslab.github.io/fastTopics/articles/relationship.html
+[vignette-scrnaseq-1]: https://stephenslab.github.io/fastTopics/articles/single_cell_rnaseq_basic.html
+[vignette-scrnaseq-2]: https://stephenslab.github.io/fastTopics/articles/single_cell_rnaseq_practical.html
